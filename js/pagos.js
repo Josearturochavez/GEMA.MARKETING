ff1e4c0b6b46fe0914ac59d861c4283592eeee0c
Jose arturo chavez yzquierdo <chavez.yzquierdo.jose.21@gmail.com>
2023-02-02 21:45:17 -0600

    se añadio el sistema de pagos en total

--- js/pagos.js
+++ js/pagos.js
@@ -1,16 +1,56 @@
 document.addEventListener('DOMContentLoaded', function () {
 	const codigo = document.getElementById('code');
 	const button = document.getElementById('search');
+	const eror = document.getElementById('eror');
 	let link = "";
+
+	
 
 	function getlink () {
 		if (codigo.value === '') {
 		}else{
 			codigo.value = codigo.value.toLowerCase();
-			link = "./pagos/pago-";
-			link = link + codigo.value + '.html';
-			console.log(link);
-			location.href = link;
+
+			function dolink () {
+				console.log("dolink iniciada");
+				link = "./pagos/pago-";
+				link = link + codigo.value + '.html';
+				console.log(link);
+				location.href = link;
+			}
+
+			if (codigo.value === 'ag-43250') {
+				dolink();
+			}else if (codigo.value === 'am-61234') {
+				dolink();
+			}else if (codigo.value === 'ap-59843') {
+				dolink();
+			}else if (codigo.value === 'mg-74632') {
+				dolink();
+			}else if (codigo.value === 'mm-94837') {
+				dolink();
+			}else if (codigo.value === 'mp-84736') {
+				dolink();
+			}else if (codigo.value === 'bg-42739') {
+				dolink();
+			}else if (codigo.value === 'bm-16238') {
+				dolink();
+			}else if (codigo.value === 'bp-04821') {
+				dolink();
+			}else if (codigo.value === 'prueba') {
+				dolink();
+			}else {
+				function zom () {
+					eror.classList.remove('zoom')
+				}
+				function dnone(){
+					eror.classList.add('d-none')
+				}
+				eror.classList.add('zoom');
+				eror.classList.remove('d-none');
+				setTimeout(zom,600)
+				setTimeout(dnone,6000)
+			};
 		};
 	};
 
